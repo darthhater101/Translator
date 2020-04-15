01ae8a3ec6201232b7123e5b492023eacd9333c7
Feschenko Ivan <razerfanat@gmail.com>
2020-04-15 22:29:19 +0300

    generator changes

--- Generator.cpp
+++ Generator.cpp
@@ -1,71 +1,140 @@
 #include "Generator.h"
+#include <string>
 
 void Generator::traversal(std::shared_ptr<struct Tree::node>& node)
 {
-	if (node == nullptr) return;
-
-	else if (node->value == "<program>")
+	if (node->value == "<program>")
 	{
-		for (auto& it : node->childs)
-		{
-			if (it->value == "<procedure-identifier>")
-			{
-				program_identifier = it->childs[0]->childs[0]->value;
-			}
-		}
+		traversal(node->childs.at(1));
+		traversal(node->childs.at(3));
+	}
+	else if (node->value == "<procedure-identifier>")
+	{
+		program_identifier = node->childs.at(0)->childs.at(0)->value;
+	}
+	else if (node->value == "<block>")
+	{
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
+		traversal(node->childs.at(2));
+		traversal(node->childs.at(3));
+	}
+	else if (node->value == "<variable-declarations>")
+	{
+		if (node->childs.at(0)->value == "<empty>")
+			return;
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
 	}
 	else if (node->value == "VAR")
 	{
-		std::cout << "DATA SEGMENT" << std::endl;
-	}
-	else if (node->value == "<declaration>")
-	{
-		for (auto& it : node->childs)
-		{
-			if (it->value == "<variable-identifier>")
-			{
-				if (std::find(identifiers.begin(), identifiers.end(), it->childs[0]->childs[0]->value) != identifiers.end())
-				{
-					std::cout << "Generator: such identifier already exist: " + it->childs[0]->childs[0]->value << "\n";
-					return;
-				}
-				std::cout << it->childs[0]->childs[0]->value << " ";
-			}
-			if (it->value == "<attribute>")
-			{
-				if(it->childs[0]->value == "INTEGER")
-					std::cout << "DW  ?\n";
-				if (it->childs[0]->value == "FLOAT")
-					std::cout << "DW  ?\n";
-			}
-		}
+		program.append("DATA SEGMENT\n");
 	}
 	else if (node->value == "<declarations-list>")
 	{
-		if (node->childs[0]->value == "<empty>")
-		{
-			std::cout << "DATA ENDS\n";
-		}
+		if (node->childs.at(0)->value == "<empty>")
+			return;
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
+	}
+	else if (node->value == "<declaration>")
+	{
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(2));
+		program.append("?\n");
+	}
+	else if (node->value == "<variable-identifier>")
+	{
+		program.append(node->childs.at(0)->childs.at(0)->value);
+	}
+	else if (node->value == "<attribute>")
+	{
+		if (node->childs.at(0)->value == "INTEGER")
+			program.append(" DW ");
+		if (node->childs.at(0)->value == "FLOAT")
+			program.append(" DW ");
 	}
 	else if (node->value == "BEGIN")
 	{
-		std::cout << "CODE SEGMENT\n\tASSUME CS:CODE, DS:DATA\n";
-		std::cout << program_identifier << ":\n";
+		program.append("CODE SEGMENT\n\tASSUME CS:CODE, DS:DATA\n");
+		program.append(program_identifier + ":\n");
 	}
 	else if (node->value == "END")
 	{
-		std::cout << "MOV AH, 4Ch\nINT 21h\nCODE ENDS\nEND " << program_identifier;
+		program.append("MOV AH, 4Ch\nINT 21h\nCODE ENDS\nEND " + program_identifier);
 	}
-
-
-	if (node->childs.empty()) return;
-	for (auto& it : node->childs)
+	else if (node->value == "<statement-list>")
 	{
-		traversal(it);
+		if (node->childs.at(0)->value == "<empty>")
+		{
+			label_index--;
+			program.append("NOP\n");
+			return;
+		}
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
+	}
+	else if (node->value == "<statement>")
+	{
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
+	}
+	else if (node->value == "<condition-statement>")
+	{
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
+	}
+	else if (node->value == "<incomplete-condition-statement>")
+	{
+		traversal(node->childs.at(1));
+		traversal(node->childs.at(3));
+	}
+	else if (node->value == "<conditional-expression>")
+	{
+		program.append("MOV AX, ");
+		traversal(node->childs.at(0));
+		program.append("\n");
+		program.append("MOV BX, ");
+		traversal(node->childs.at(2));
+		program.append("\n");
+		program.append("CMP AX, BX\n");
+		traversal(node->childs.at(1));
+	}
+	else if (node->value == "<expression>")
+	{
+		traversal(node->childs.at(0));
+	}
+	else if (node->value == "<unsigned-integer>")
+	{
+		program.append(node->childs.at(0)->value);
+	}
+	else if (node->value == "=")
+	{
+		program.append("JNE ?L" + std::to_string(label_index));
+		program.append("\n");
+	}
+	else if (node->value == "<alternative-part>")
+	{
+		if (node->childs.at(0)->value == "<empty>")
+		{
+			return;
+		}
+		traversal(node->childs.at(0));
+		traversal(node->childs.at(1));
+	}
+	else if (node->value == "ELSE")
+	{
+		program.append("JMP ?L" + std::to_string(label_index));
+		program.append("\n");
+	}
+	else if (node->value == "ENDIF")
+	{
+		return;
 	}
 }
 
 void Generator::traversal()
 {
-	this->traversal(tree.root);
+	this->traversal(tree.root->childs.at(0));
 }
+
--- Generator.h
+++ Generator.h
@@ -3,6 +3,7 @@
 #include "InformationTables.h"
 #include <memory>
 #include <vector>
+#include <stack>
 
 class Generator
 {
@@ -12,10 +13,12 @@ private:
 	std::vector<std::string> identifiers;
 	std::string program_identifier;
 	void traversal(std::shared_ptr<struct Tree::node>& node);
+	int label_index = -1;
+	std::string program;
 public:
 	Generator() {}
 	Generator(const Tree& tree, const std::shared_ptr<InformationTables>& tables) : tree(tree), tables(tables) {}
 	void traversal();
-
+	void print() { std::cout << program; }
 };
 
